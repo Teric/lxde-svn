2d2ea8ff6077a883151d4f12d4e34cc728237f66
thiefghost <thiefghost@3baf7735-d320-0410-8b56-b11ef3703fae>
2008-05-14 06:44:28 +0000

    some fixes
    git-svn-id: https://lxde.svn.sourceforge.net/svnroot/lxde/trunk@647 3baf7735-d320-0410-8b56-b11ef3703fae

--- lxnm/data/wificonnect.sh
+++ lxnm/data/wificonnect.sh
@@ -10,9 +10,9 @@ if [ A"$LXNM_WIFI_PROTO" = A"NONE" ]; then
 	# without encryption
 	ifconfig $LXNM_IFNAME up
 	if [ A"$LXNM_WIFI_ESSID" = A ]; then
-		iwconfig $LXNM_IFNAME essid '$LXNM_WIFI_ESSID' key off
+		iwconfig $LXNM_IFNAME essid "$LXNM_WIFI_ESSID" key off
 	else
-		iwconfig $LXNM_IFNAME ap $LXNM_WIFI_APADDR key off
+		iwconfig $LXNM_IFNAME ap "$LXNM_WIFI_APADDR" key off
 	fi
 
 	dhclient $LXNM_IFNAME -1 -d -pf /var/run/dhclient_$LXNM_IFNAME.pid
@@ -25,9 +25,7 @@ elif [ A"$LXNM_WIFI_PROTO" = A"WEP" ]; then
 	# WEP
 	ifconfig $LXNM_IFNAME up
 	if [ A"$LXNM_WIFI_ESSID" = A ]; then
-		iwconfig $LXNM_IFNAME essid '$LXNM_WIFI_ESSID' key 's:$LXNM_WIFI_KEY'
-	else
-		iwconfig $LXNM_IFNAME ap $LXNM_WIFI_APADDR key 's:$LXNM_WIFI_KEY'
+		iwconfig $LXNM_IFNAME essid "$LXNM_WIFI_ESSID" key "s:$LXNM_WIFI_KEY"
 	fi
 
 	dhclient $LXNM_IFNAME -1 -d -pf /var/run/dhclient_$LXNM_IFNAME.pid
@@ -44,12 +42,12 @@ else
 	wpa_cli -g/var/run/wpa_supplicant-global interface_add $LXNM_IFNAME "" wext /var/run/wpa_supplicant
 
 	wpa_cli -i$LXNM_IFNAME add_network
-	wpa_cli -i$LXNM_IFNAME set_network 0 ssid '$LXNM_WIFI_ESSID'
-	wpa_cli -i$LXNM_IFNAME set_network 0 key_mgmt $LXNM_WIFI_KEYMGMT
-	wpa_cli -i$LXNM_IFNAME set_network 0 psk '"$LXNM_WIFI_KEY"'
-	wpa_cli -i$LXNM_IFNAME set_network 0 pairwise $LXNM_WIFI_PAIRWISE
-	wpa_cli -i$LXNM_IFNAME set_network 0 group $LXNM_WIFI_GROUP
-	wpa_cli -i$LXNM_IFNAME set_network 0 proto $LXNM_WIFI_PROTO
+	wpa_cli -i$LXNM_IFNAME set_network 0 ssid "$LXNM_WIFI_ESSID"
+	wpa_cli -i$LXNM_IFNAME set_network 0 key_mgmt "$LXNM_WIFI_KEYMGMT"
+	wpa_cli -i$LXNM_IFNAME set_network 0 psk "$LXNM_WIFI_KEY"
+	wpa_cli -i$LXNM_IFNAME set_network 0 pairwise "$LXNM_WIFI_PAIRWISE"
+	wpa_cli -i$LXNM_IFNAME set_network 0 group "$LXNM_WIFI_GROUP"
+	wpa_cli -i$LXNM_IFNAME set_network 0 proto "$LXNM_WIFI_PROTO"
 	wpa_cli -i$LXNM_IFNAME enable_network 0
 
 	dhclient $LXNM_IFNAME -1 -d -pf /var/run/dhclient_$LXNM_IFNAME.pid
